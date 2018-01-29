fed752dd2e26c1e79fb71d423bab273e61aac35e
jbi <tobilon@outlook.com>
2018-01-29 21:24:23 +0800

    add rlp decode test case

--- rlp/rlp_test.go
+++ rlp/rlp_test.go
@@ -1,20 +1,40 @@
 package rlp
 
 import (
+	"bytes"
 	"testing"
 )
 
-func TestEncode(t *testing.T) {
+func TestCodec(t *testing.T) {
 	var a byte
+
+	// 1 byte 0
+	a = 0
+	data, err := EncodeToBytes(a)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(data) != 1 || data[0] != 128+a {
+		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", data[0], a, len(data))
+	}
 
 	// 1 byte 1~127
 	a = 127
-	data, err := EncodeToBytes(a)
+	data, err = EncodeToBytes(a)
 	if err != nil {
 		t.Error(err)
 	}
 	if len(data) != 1 || data[0] != a {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", data[0], a, len(data))
+	}
+
+	var res byte
+	err = Decode(bytes.NewReader(data), &res)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != 127 {
+		t.Errorf("Decode want 0x%x, but get 0x%x", a, res)
 	}
 
 	// 1 byte 128~255
@@ -26,6 +46,13 @@ func TestEncode(t *testing.T) {
 	if len(data) != 2 || data[1] != a || data[0] != 129 {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", a, data, len(data))
 	}
+	err = Decode(bytes.NewReader(data), &res)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != 128 {
+		t.Errorf("Decode want 0x%x, but get 0x%x", a, res)
+	}
 
 	a = 255
 	data, err = EncodeToBytes(a)
@@ -34,6 +61,14 @@ func TestEncode(t *testing.T) {
 	}
 	if len(data) != 2 || data[1] != a || data[0] != 129 {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", a, data, len(data))
+	}
+
+	err = Decode(bytes.NewReader(data), &res)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != 255 {
+		t.Errorf("Decode want 0x%x, but get 0x%x", a, res)
 	}
 
 	// byte array lenght < 56
@@ -46,6 +81,20 @@ func TestEncode(t *testing.T) {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", b, data, len(data))
 	}
 
+	var resArray []byte
+	err = Decode(bytes.NewReader(data), &resArray)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(resArray) != len(b) {
+		t.Errorf("Decode length want 0x%x, but get 0x%x", len(b), len(resArray))
+	}
+	for i, r := range resArray {
+		if r != b[i] {
+			t.Errorf("Decode want 0x%x, but get 0x%x", b[i], r)
+		}
+	}
+
 	b = []byte{54: 0}
 	data, err = EncodeToBytes(b)
 	if err != nil {
@@ -53,6 +102,19 @@ func TestEncode(t *testing.T) {
 	}
 	if len(data) != len(b)+1 || data[0] != 128+byte(len(b)) {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", b, data, len(data))
+	}
+
+	err = Decode(bytes.NewReader(data), &resArray)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(resArray) != len(b) {
+		t.Errorf("Decode length want 0x%x, but get 0x%x", len(b), len(resArray))
+	}
+	for i, r := range resArray {
+		if r != b[i] {
+			t.Errorf("Decode want 0x%x, but get 0x%x", b[i], r)
+		}
 	}
 
 	// byte array lenght >= 56
@@ -65,6 +127,19 @@ func TestEncode(t *testing.T) {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", b, data, len(data))
 	}
 
+	err = Decode(bytes.NewReader(data), &resArray)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(resArray) != len(b) {
+		t.Errorf("Decode length want 0x%x, but get 0x%x", len(b), len(resArray))
+	}
+	for i, r := range resArray {
+		if r != b[i] {
+			t.Errorf("Decode want 0x%x, but get 0x%x", b[i], r)
+		}
+	}
+
 	b = []byte{99: 0}
 	data, err = EncodeToBytes(b)
 	if err != nil {
@@ -72,5 +147,18 @@ func TestEncode(t *testing.T) {
 	}
 	if len(data) != len(b)+1+1 || data[0] != byte(0xb7+1) || data[1] != byte(len(b)) {
 		t.Errorf("EncodeToBytes want 0x%x, but get 0x%x, len %d", b, data, len(data))
+	}
+
+	err = Decode(bytes.NewReader(data), &resArray)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(resArray) != len(b) {
+		t.Errorf("Decode length want 0x%x, but get 0x%x", len(b), len(resArray))
+	}
+	for i, r := range resArray {
+		if r != b[i] {
+			t.Errorf("Decode want 0x%x, but get 0x%x", b[i], r)
+		}
 	}
 }
