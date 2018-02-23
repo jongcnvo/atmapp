2e386b98dfce70ae39c876ea4c6cca1598b81aa2
jbi <tobilon@outlook.com>
2018-02-23 14:47:58 +0800

    test filepath import

--- test/main.go
+++ test/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"../rlp"
 	"fmt"
+	"math/big"
 )
 
 func main() {
-	fmt.Printf("Hello, world!")
+	var td big.Int
+	td.SetInt64(1)
+	data, err := rlp.EncodeToBytes(td)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("0x%x", data)
 }
